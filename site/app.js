f8d9380f20c39c66d4fbddea05357fec78e9f66a
TORUN PNEUS <gabriellydamaiam@gmail.com>
2026-09-10 17:19:33 -0300

    Remove código morto: const SAIDA_TIPOS, 8 regras CSS sem uso (bell-dot, cliente-nome-click, cliente-tags-list-compact, dash-expand-hint, kanban-dragover, pill-empty, rank-pos, rank-sub), símbolo SVG i-external

--- site/app.js
+++ site/app.js
@@ -36,8 +36,6 @@ const TIPO_LABEL = {
   avariado: "Avariado"
 };
 
-const SAIDA_TIPOS = ["venda", "reserva", "transferencia", "avariado"];
-
 const MES_ABREV = {
   "01": "Jan", "02": "Fev", "03": "Mar", "04": "Abr", "05": "Mai", "06": "Jun",
   "07": "Jul", "08": "Ago", "09": "Set", "10": "Out", "11": "Nov", "12": "Dez"
